6d3ec5d6c2b491b8daa070fab7858448ff2658fb
Yandy Sanchez Orosa <skullcandy961205@gmail.com>
2024-01-28 17:46:02 -0500

    Introduccion a React

---
+++ src/useBreedList.jsx
@@ -0,0 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedList";
+
+export default function useBreedList(animal) {
+    const results = useQuery({ queryKey : animal, queryFn : fetchBreedList });
+
+    return [results?.data?.breeds ?? [], results.status];
+}
